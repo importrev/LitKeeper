abf2bcd181537dd55711b15e10026c4a4a4e06b5
importrev <github@xyzabc.cyou>
2025-11-21 19:45:29 +0100

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -32,7 +32,7 @@ def background_process_url(app, url):
                 story_content, 
                 os.path.join(os.path.dirname(__file__), "data", "epubs"),
                 story_category=story_category,
-                story_tags=story_tags
+                story_tags=story_tags,
                 description_text=download_story(url)
             )
             log_action(f"Successfully created EPUB file: {epub_file_name}")
